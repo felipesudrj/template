cf47521356f645216c732b45854b6f0b7e981ba2
felipesudrj <felipe.devel@gmail.com>
2014-11-13 16:04:26 -0200

    Create UnidadeMedida.php

---
+++ model/php/UnidadeMedida.php
@@ -0,0 +1,16 @@
+<?php 
+
+//UnidadeMedida(unidade_medida_id,descricao)
+
+App::uses('AppModel','Model');
+class UnidadeMedida extends AppModel {    
+    
+    public $useTable = 'unidade_medida';
+	public $primaryKey = 'unidade_medida_id';
+    
+	
+	
+}
+
+
+?>
