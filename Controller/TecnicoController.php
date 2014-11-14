679e28a43e43dbbb61dfdbc63b9109d0a6b02939
felipesudrj <felipe.devel@gmail.com>
2014-11-14 02:35:51 -0200

    controller tecnico

--- Controller/TecnicoController.php
+++ Controller/TecnicoController.php
@@ -14,31 +14,23 @@ App::uses('AppController', 'Controller');
  */
 class TecnicoController extends AppController {
 
-    public $uses = array('Usuario', 'Tecnico','TipoUsuario');
+    public $uses = array('Usuario', 'Tecnico', 'TipoUsuario');
 
     public function cadastrar($id = null) {
 
         if ($this->request->is('post')) {
+            $dados = $this->request->data;
+            $dados['Usuario']['nome'] = $dados['Tecnico']['nome'];
 
-            
+
             $this->Tecnico->set($this->request->data);
             $this->Usuario->set($this->request->data);
             if ($this->Tecnico->validates() && $this->Usuario->validates()) {
-            
-                if(isset($this->request->data['Usuario']['senha'])){
-            $this->request->data['Usuario']['senha'] = AuthComponent::password($this->request->data['Usuario']['senha']);
-            }
-                
-                $this->request->data['Usuario']['nome'] = $this->request->data['Tecnico']['nome'];
-                
+
                 try {
-                    if(empty($this->request->data['Usuario']['usuario_id'])){
-                        unset($this->request->data['Usuario']['usuario_id']);
-                         unset($this->request->data['Tecnico']['tecnico_id']);
-                    }
-                    pr($this->request->data);
-                    $this->Usuario->saveAll($this->request->data);
-                    die('salvou');
+     
+                    $this->Usuario->saveAll($dados);
+                    
                     $this->Session->setFlash("Salvo com sucesso", false, false, 'confirma');
                     $this->redirect($this->here);
                 } catch (Exception $e) {
@@ -46,24 +38,21 @@ class TecnicoController extends AppController {
                     $this->Session->setFlash($msg, false, false, 'negar');
                     $this->redirect($this->here);
                 }
-            }else{
-                    $erros = $this->Tecnico->validationErrors;
-                    $erros = $this->Usuario->validationErrors;
-                    $msg = "Alguns campos obrigatórios não foram preenchidos";
-                    $this->Session->setFlash($msg, false, false, 'atencao');
-                    $this->set('erros',$erros);
-
+            } else {
+                $erros = $this->Tecnico->validationErrors;
+                $erros = $this->Usuario->validationErrors;
+                $msg = "Alguns campos obrigatórios não foram preenchidos";
+                $this->Session->setFlash($msg, false, false, 'atencao');
+                $this->set('erros', $erros);
             }
-        } 
+        }
 
-            $tipoUsuario = $this->TipoUsuario->find('list',array('fields'=>array('tipo_usuario_id','descricao')));
-            $this->set('tipoUsuario',$tipoUsuario);
-            if (!empty($id)) {
-                $this->request->data = $this->Usuario->findByusuario_id($id);
-                pr($this->request->data);
-                
-            }
-        
+        $tipoUsuario = $this->TipoUsuario->find('list', array('fields' => array('tipo_usuario_id', 'descricao')));
+        $this->set('tipoUsuario', $tipoUsuario);
+        if (!empty($id)) {
+            $this->request->data = $this->Usuario->findByusuario_id($id);
+            pr($this->request->data);
+        }
     }
 
     public function editar() {
