6c4c276d1a7bf7844c732fd61d715e2cbd5f2202
felipesudrj <felipe.devel@gmail.com>
2014-11-21 16:18:46 -0200

    Update RelatorioController.php

--- Controller/RelatorioController.php
+++ Controller/RelatorioController.php
@@ -14,9 +14,41 @@ App::uses('AppController', 'Controller');
  */
 class RelatorioController extends AppController {
 
-    public function atendimento() {
-        
-    }
-    public function estoque(){}
-
+    public function atendimento() {	
+	
+	if($this->request->is('post')){
+			/*PEGAR VIEW DE IMPRESSÃO */
+			
+			/* MOSTRAR TODOS OS ATENDIMENTOS DO TÉCNICO EM LISTA CONFORME 
+			DATA AGENDADA, TECNICO ID E STATUS DE ATENDIMENTO */
+					
+	}
+	
+	}
+    public function equipamento(){
+	
+	
+	if($this->request->is('post')){
+			/*PEGAR VIEW DE IMPRESSÃO */
+			
+			/* MOSTRAR EM LISTA DADOS DOS EQUIPAMENTOS QUE O TÉCNICO 
+			POSSUI CONFORME DISTRIBUICAO - UTILIZADO 
+			FILTRAR POR DATA DE RETIRADA E TECNICO 
+			*/
+			
+			/* VER O TOTAL DE ITENS DISTRIBUIDOS SUBTRAIR PELO TOTAL DE UTILIZADOS EXIBIR NA TELA AS 3
+				INFORMAÇOES, TOTAL RETIRADO, UTILIZADO, SALDO */
+					
+	}
+	
+	}
+	public function ordem($atendimento_id){
+	
+	/*	PEGAR DADOS DE ATENDIMENTO E IMPRIMIR EM TELA */
+	
+	}
+	public function importacao(){
+	/*MOSTRAR TODAS AS IMPORTACOES FILTRAR POR DATA DE IMPORTACAO E TIPO */
+	
+	}
 }
